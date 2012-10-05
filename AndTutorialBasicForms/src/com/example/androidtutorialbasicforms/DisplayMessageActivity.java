93488093028b3c62aaa25cbd261deb733998f22a
Travis Bybee <travis.bybee@gmail.com>
2012-10-04 20:30:45 -0600

    Added in Send button functionality and a new activity for the button.

---
+++ AndTutorialBasicForms/src/com/example/androidtutorialbasicforms/DisplayMessageActivity.java
@@ -0,0 +1,20 @@
+package com.example.androidtutorialbasicforms;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class DisplayMessageActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_display_message, menu);
+        return true;
+    }
+}
--- AndTutorialBasicForms/src/com/example/androidtutorialbasicforms/MainActivity.java
+++ AndTutorialBasicForms/src/com/example/androidtutorialbasicforms/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.androidtutorialbasicforms;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
+	public final static String EXTRA_MESSAGE = "com.example.BasicForms.MESSAGE";
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +21,12 @@ public class MainActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
+    }
+    
+    public void sendMessage(View view) {
+    	Intent intent = new Intent(this, DisplayMessageActivity.class);
+    	EditText editText = (EditText)findViewById(R.id.edit_message);
+    	intent.putExtra(EXTRA_MESSAGE, editText.getText().toString());
+    	startActivity(intent);
     }
 }

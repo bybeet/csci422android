d5ffe1552fd906a825ce441fedbd26dd285d9010
Travis Bybee <travis.bybee@gmail.com>
2012-10-09 22:02:19 -0600

    Added gestures, game activity, and finished the project.

--- And-Gestures-Tutorial/src/com/example/and_gestures_tutorial/MainActivity.java
+++ And-Gestures-Tutorial/src/com/example/and_gestures_tutorial/MainActivity.java
@@ -1,20 +1,62 @@
 package com.example.and_gestures_tutorial;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.gesture.Gesture;
+import android.gesture.GestureLibraries;
+import android.gesture.GestureLibrary;
+import android.gesture.GestureOverlayView;
+import android.gesture.GestureOverlayView.OnGesturePerformedListener;
+import android.gesture.Prediction;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+import com.bybeet.and_gestures_tutorial.R;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+public class MainActivity extends Activity implements OnGesturePerformedListener {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
+	GestureLibrary mLibrary;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		mLibrary = GestureLibraries.fromRawResource(this, R.raw.gestures);
+		if(!mLibrary.load())
+			finish();
+
+		GestureOverlayView gestures = (GestureOverlayView)findViewById(R.id.gestures);
+		gestures.addOnGesturePerformedListener(this);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+	@Override
+	public void onGesturePerformed(GestureOverlayView overlay, Gesture gesture) {
+		ArrayList<Prediction> predictions = mLibrary.recognize(gesture);
+
+		if(predictions.size() > 0 && predictions.get(0).score > 1.0) {
+			String result = predictions.get(0).name;
+
+			if("open".equalsIgnoreCase(result)){
+				Toast.makeText(this, "Opening the document",  Toast.LENGTH_LONG).show();
+			}else if("save".equalsIgnoreCase(result)){
+				Toast.makeText(this, "Saving the document", Toast.LENGTH_LONG).show();
+			}else if("star".equalsIgnoreCase(result)){
+				Intent intent = new Intent(this, com.bybeet.and_gestures_tutorial.GameActivity.class);
+				startActivity(intent);
+			}else if("toast".equalsIgnoreCase(result)){
+				Toast.makeText(this, "You made toast!!!", Toast.LENGTH_LONG).show();
+			}
+		}
+
+	}
 }

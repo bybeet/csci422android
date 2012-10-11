5bc796ba4726c770f3984f1059bedf21c38db408
Travis Bybee <travis.bybee@gmail.com>
2012-10-10 20:41:14 -0600

    Implemented gesture actions in GestureListener subclass. Added images to the drawable folder.

--- And-Fling/src/com/bybeet/and_fling/MainActivity.java
+++ And-Fling/src/com/bybeet/and_fling/MainActivity.java
@@ -3,11 +3,15 @@ package com.bybeet.and_fling;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Matrix;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.GestureDetector;
+import android.view.GestureDetector.OnDoubleTapListener;
 import android.view.Menu;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.FrameLayout;
 
@@ -34,9 +38,13 @@ public class MainActivity extends Activity {
 		private static final String DEBUG_TAG = "PlayAreaView_onDraw";
     	private Matrix translate;
     	private Bitmap droid;
+    	private GestureDetector gestures;
     	
     	public PlayAreaView(Context context) {
 			super(context);
+			translate = new Matrix();
+			gestures = new GestureDetector(MainActivity.this, new GestureListener(this));
+			droid = BitmapFactory.decodeResource(getResources(), R.drawable.ic_android);
 			// TODO Auto-generated constructor stub
 		}
     	
@@ -47,5 +55,96 @@ public class MainActivity extends Activity {
     		Log.d(DEBUG_TAG,"Matrix: " + translate.toShortString());
     		Log.d(DEBUG_TAG,"Canvas: " + m.toShortString());
     	}
+    	
+
+		public void onMove(float dx, float dy) {
+			translate.postTranslate(dx, dy);
+			invalidate();
+		}
+		
+		public void onResetLocation() {
+			// TODO Auto-generated method stub
+			
+		}
+    	
+    	@Override
+    	public boolean onTouchEvent(MotionEvent event){
+    		return gestures.onTouchEvent(event);
+    	}
+    	
+    	private class GestureListener implements GestureDetector.OnGestureListener, OnDoubleTapListener {
+    		
+    		private static final String DEBUG_TAG = "GestureListener";
+    		private PlayAreaView view;
+    		
+    		public GestureListener (PlayAreaView view) {
+    			this.view = view;
+    		}
+    		
+    		@Override
+    		public boolean onDown(MotionEvent e) {
+    			Log.v(DEBUG_TAG, "onDown");
+    			return true;
+    		}
+
+			@Override
+			public boolean onDoubleTap(MotionEvent e) {
+				// TODO Auto-generated method stub
+				return false;
+			}
+
+			@Override
+			public boolean onDoubleTapEvent(MotionEvent e) {
+				Log.v(DEBUG_TAG, "onDoubleTap");
+				view.onResetLocation();
+				return true;
+			}
+
+			@Override
+			public boolean onSingleTapConfirmed(MotionEvent e) {
+				// TODO Auto-generated method stub
+				return false;
+			}
+
+			@Override
+			public boolean onFling(MotionEvent e1, MotionEvent e2,
+					float velocityX, float velocityY) {
+				Log.v(DEBUG_TAG, "onFling");
+				
+				final float distanceTimeFactor = 0.4f;
+				final float totalDx = (distanceTimeFactor * velocityX/2);
+				final float totalDy = (distanceTimeFactor * velocityY/2);
+				
+				view.onAnimateMove(totalDx, totalDy, (long)(1000 * distanceTimeFactor));
+				return true;
+			}
+
+			@Override
+			public void onLongPress(MotionEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public boolean onScroll(MotionEvent e1, MotionEvent e2,
+					float distanceX, float distanceY) {
+				Log.v(DEBUG_TAG, "onScroll");
+				view.onMove(-distanceX, -distanceY);
+				return true;
+			}
+
+			@Override
+			public void onShowPress(MotionEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public boolean onSingleTapUp(MotionEvent e) {
+				// TODO Auto-generated method stub
+				return false;
+			}
+    	}
+
     }
 }
